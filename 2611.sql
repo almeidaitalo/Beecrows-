e24d1260fc0dbfb9ab9a07cda74bc36dd8e7f29d
Ítalo  ⚡️ <80206695+almeidaitalo@users.noreply.github.com>
2022-07-03 16:16:48 -0300

    Update 2611.sql

--- 2611.sql
+++ 2611.sql
@@ -1,3 +1,3 @@
 select mov.id, mov.name
-from movies mov inner join genres gen on mov.id_genres = gen.id	
+from movies mov join genres gen on mov.id_genres = gen.id	
 where lower (gen.description) = 'action'
